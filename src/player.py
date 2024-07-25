de7c52a35df9fcbbbfc9997c0077e2268cbeed71
h268081 <h268081@stud.u-szeged.hu>
2024-07-25 18:32:09 +0200

    added docstrings to functions for later usages

--- src/player.py
+++ src/player.py
@@ -8,6 +8,21 @@ class Status(Enum):
 
 class Player:
     def __init__(self, name, money):
+        """
+        Initializes a new instance of the Player class.
+
+        :param name: A string representing the name of the player.
+        :param money: An integer representing the amount of money the player has.
+
+        This method initializes a new instance of the Player class. It sets the initial values for the instance variables `name`, `cards`, `money`, `status`, and `current_bet`.
+
+        Parameters:
+            name (str): The name of the player.
+            money (int): The amount of money the player has.
+
+        Returns:
+            None
+        """
         self.name = name
         self.cards = []
         self.money = money
@@ -15,15 +30,50 @@ class Player:
         self.current_bet = 0
 
     def bet(self, amount):
+        """
+        Decreases the player's money by the given amount and increases their current bet by the same amount.
+
+        Parameters:
+            amount (int): The amount of money to bet.
+
+        Raises:
+            ValueError: If the amount of money to bet is greater than the player's current money.
+
+        Returns:
+            None
+        """
         if amount > self.money:
             raise ValueError("Cannot bet more money than the player has")
         self.money -= amount
         self.current_bet += amount
 
     def fold(self):
+        """
+        Sets the status of the player to FOLDED.
+
+        This function sets the `status` attribute of the `Player` object to `Status.FOLDED`.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self.status = Status.FOLDED
 
     def go_all_in(self):
+        """
+        Sets the player's current bet to their entire balance and sets their status to ALL_IN.
+
+        This function increases the player's current bet by their entire balance and sets their money balance to 0.
+        It also sets their status to Status.ALL_IN, indicating that the player has gone all-in.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self.current_bet += self.money
         self.money = 0
         self.status = Status.ALL_IN
--- src/table.py
+++ src/table.py
@@ -1,6 +1,6 @@
 from player import Player
-
 from enum import Enum, auto
+import random
 
 class Rank(Enum):
     TWO = 2
@@ -26,31 +26,95 @@ class Suit(Enum):
 
 class Table:
     def __init__(self):
+        """
+        Initializes a new instance of the `Table` class.
+
+        This method initializes a new instance of the `Table` class. It sets the initial values for the instance variables `players`, `com_cards`, `pot`, and `deck`.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self.players = []
         self.com_cards = []
         self.pot = 0
         self.deck = self.create_deck()
 
     def create_deck(self):
+        """
+        Creates a new deck of cards.
+
+        Returns:
+            list: A list of tuples representing each card in the deck. Each tuple contains the rank and suit of a card.
+        """
         deck = [(rank, suit) for suit in Suit for rank in Rank]
         return deck
 
     def shuffle_deck(self):
-        import random
+        """
+        Shuffles the deck of cards.
+
+        This method shuffles the deck of cards by using the `random.shuffle()` function from the `random` module. It takes the `self.deck` list, which represents the deck of cards, and shuffles it in place.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         random.shuffle(self.deck)
 
     def add_player(self, player):
+        """
+        Adds a player to the list of players.
+
+        Parameters:
+            player (Player): The player to be added.
+
+        Returns:
+            None
+        """
         self.players.append(player)
 
     def add_to_pot(self, amount):
+        """
+        Adds the given amount to the pot.
+
+        Args:
+            amount (float): The amount to be added to the pot.
+
+        Returns:
+            None
+        """
         self.pot += amount
         
     def deal_community_cards(self, num_cards):
+        """
+        Deals a specified number of community cards from the deck and adds them to the list of community cards.
+
+        Parameters:
+            num_cards (int): The number of community cards to deal.
+
+        Returns:
+            None
+        """
         for _ in range(num_cards):
             card = self.deck.pop()
             self.com_cards.append(card)
 
     def deal_to_player(self, player, num_cards):
+        """
+        Deals a specified number of cards to a player from the deck.
+
+        Args:
+            player (Player): The player to whom the cards will be dealt.
+            num_cards (int): The number of cards to deal to the player.
+
+        Returns:
+            None
+        """
         for _ in range(num_cards):
             card = self.deck.pop()
             player.cards.append(card)
